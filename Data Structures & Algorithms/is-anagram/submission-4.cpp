3f44f84844100f952a6a8885a854ee993f0d0739
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2026-07-28 18:19:20 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+        return false;
+
+        vector<int>freq(26,0);
+        for(char c:s){
+            freq[c-'a']++;
+        }
+        for(char c:t){
+            freq[c-'a']--;
+        }
+        for(int cnt :freq){
+            if(cnt!=0)
+            return false;
+        }
+        return true;
+    }
+};
