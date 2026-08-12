422d6568593e0d4d7b3c87c1a48366a158aac7c6
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2026-08-12 16:07:53 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+vector<string>ans;
+void backtrack(int n,int open,int close,string& curr){
+    if(curr.size()==2*n){
+        ans.push_back(curr);
+        return;
+    }
+    if(open<n){
+        curr.push_back('(');
+        backtrack(n,open+1,close,curr);
+        curr.pop_back();
+
+    }
+
+    if(close<open){
+        curr.push_back(')');
+        backtrack(n,open,close+1,curr);
+
+        curr.pop_back();
+    }
+}
+    vector<string> generateParenthesis(int n) {
+        string curr;
+        backtrack(n,0,0,curr);
+
+        return ans;
+    }
+};
